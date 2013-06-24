d1b793ca64fa0eae6090ce3c1f4109ab798726fe
Abdulla Elyas <a@elyas.de>
2013-06-24 03:19:33 +0100

    Added rPiArchinstallAsusUSB-N10.sh & rPiArchRunWlan.sh

---
+++ rPiArchRunWlan.sh
@@ -0,0 +1,3 @@
+sudo ip link set wlan0 up
+sudo wifi-menu -o
+sudo netctl enable
---
+++ rPiArchinstallAsusUSB-N10.sh
@@ -0,0 +1,5 @@
+sudo pacman -Syyu
+sudo reboot
+lsmod | grep r8712u
+lsusb | grep ASUSTek
+sudo pacman -Syy wireless_tools
